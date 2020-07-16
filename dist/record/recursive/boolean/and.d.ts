acb4d782f9ce2d32d83999e50cde0d00dc1658c3
dikac <rigel234@yahoo.com>
2020-07-17 01:55:57 +0700

    move "record" component to "t-object"

--- dist/record/recursive/boolean/and.d.ts
+++
@@ -1,3 +0,0 @@
-import Record from "@dikac/t-object/record/recursive/record";
-import Validatable from "../../../validatable";
-export default function And<Object extends Record<PropertyKey, Validatable>>(object: Object): boolean;
--- dist/record/recursive/invalid.d.ts
+++
@@ -1,7 +0,0 @@
-import Validatable from "../../validatable";
-import Record from "@dikac/t-object/record/recursive/record";
-import { O } from "ts-toolbelt";
-/**
- * filter all invalid {@link Validatable} while retain its original structure
- */
-export default function Invalid<V extends Validatable = Validatable, Object extends Record<keyof any, V> = Record<keyof any, V>>(record: Object): O.Partial<Object, 'deep'>;
