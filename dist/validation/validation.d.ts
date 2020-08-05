67fe35a609f114696249af3ac14b4640efe83519
dikac <rigel234@yahoo.com>
2020-08-05 18:07:45 +0700

    dependency update, add validation

---
+++ dist/validation/validation.d.ts
@@ -0,0 +1,5 @@
+import Function from "@dikac/t-function/function";
+import Validatable from "../validatable";
+export default interface Validation<FunctionT extends Function<unknown[], Validatable>> {
+    validation: FunctionT;
+}
